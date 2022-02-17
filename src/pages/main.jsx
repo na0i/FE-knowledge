9c429fa8d52c36e893b33e16d65564c20535b994
na_0_i <nayoi1oo5@gmail.com>
2022-02-17 23:59:11 +0900

    Initial commit

---
+++ src/pages/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+	return (
+		<div>
+			<div>당신을 위한 맞춤형 학술정보서비스</div>
+			<div>AI로 더 똑똑해진 서비스를 이용해보세요.</div>
+			<input></input>
+			<div></div>
+		</div>
+	);
+};
+
+export default Main;
