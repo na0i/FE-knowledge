1c2d382e191046f812af2770fc98a002f647537b
na_0_i <nayoi1oo5@gmail.com>
2022-02-18 12:59:51 +0900

    add main style

--- src/components/recentPaperListItem.jsx
+++ src/components/recentPaperListItem.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from 'styled-components';
+
+const PaperItem = styled.div`
+  display: flex;
+  margin: 30px 0px;
+  justify-content: space-between;
+`
+
+const PaperTitle = styled.span`
+  width: 40%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
 
 const RecentPaperListItem = (paper) => {
   return(
-  <div>
-    <span>{paper.title}</span>
+  <PaperItem
+  >
+    <PaperTitle>{paper.title}</PaperTitle>
     <span>{paper.author} {paper.releaseYear}</span>
-
-  </div>);
+  </PaperItem>);
 };
 
 export default RecentPaperListItem;
