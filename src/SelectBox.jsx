412a539eeb1039a09fb591266be42e96fbe7d7f4
na_0_i <nayoi1oo5@gmail.com>
2022-03-25 15:36:54 +0900

    add: tooltip

--- src/SelectBox.jsx
+++ src/SelectBox.jsx
@@ -1,35 +1,68 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const SELECTBOX_DATA = [
-  { id: '1', value: '음성텍스트로 받아쓰기', desc: '음성을 입력하면 자동으로 텍스트로 변환해줘요:)' },
-  { id: '2', value: '영상 자막 생성하기', desc: '영상 음성을 분석해 ai가 자동으로 자막을 생성해줘요:)' },
-  { id: '3', value: '음성으로 검색하기', desc: '음성으로 검색하는 기능이 필요한 경우 사용해 보세요:)' },
+  {
+    id: "1",
+    value: "음성텍스트로 받아쓰기",
+    desc: "음성을 입력하면 자동으로 텍스트로 변환해줘요:)",
+  },
+  {
+    id: "2",
+    value: "영상 자막 생성하기",
+    desc: "영상 음성을 분석해 ai가 자동으로 자막을 생성해줘요:)",
+  },
+  {
+    id: "3",
+    value: "음성으로 검색하기",
+    desc: "음성으로 검색하는 기능이 필요한 경우 사용해 보세요:)",
+  },
 ];
 
-function SelectBox () {
-  const [isSelectBoxActive, setIsSelectBoxActive] = useState(false);
-  const onTrigger = () => {
-    setIsSelectBoxActive(!isSelectBoxActive);
-  }
+function SelectBox() {
+  const [isSTTSelectBoxActive, setIsSTTSelectBoxActive] = useState(false);
+  const [isTTSSelectBoxActive, setIsTTSSelectBoxActive] = useState(false);
+  const onSTTTrigger = () => {
+    setIsSTTSelectBoxActive(!isSTTSelectBoxActive);
+    setIsTTSSelectBoxActive(isSTTSelectBoxActive);
+  };
+  const onTTSTrigger = () => {
+    setIsTTSSelectBoxActive(!isTTSSelectBoxActive);
+    setIsSTTSelectBoxActive(isTTSSelectBoxActive);
+  };
 
-  return(
+  return (
     <>
       <div>
-       <TriggerButton onClick={onTrigger}>STT</TriggerButton>
-      </div>
-      {isSelectBoxActive? (
-      <SelectBoxContainer>
-        {SELECTBOX_DATA.map((el) =>
-          <SelectBoxButton
-            key={el.id}
-            title={el.desc}
-          >{el.value}</SelectBoxButton>
+        <TriggerButton onClick={onSTTTrigger}>STT</TriggerButton>
+        <TriggerButton onClick={onTTSTrigger}>TTS</TriggerButton>
+        {isSTTSelectBoxActive ? (
+          <SelectBoxContainer>
+            {SELECTBOX_DATA.map((el) => (
+              <SelectBoxButton key={el.id}>
+                <span>
+                  {el.value}
+                  <ToolTip>{el.desc}</ToolTip>
+                </span>
+                {/* <ToolTip>{el.desc}</ToolTip> */}
+              </SelectBoxButton>
+            ))}
+          </SelectBoxContainer>
+        ) : (
+          <></>
         )}
-      </SelectBoxContainer>
-      ) : <></> }
+        {isTTSSelectBoxActive ? (
+          <SelectBoxContainer>
+            {SELECTBOX_DATA.map((el) => (
+              <SelectBoxButton key={el.id}>TTS</SelectBoxButton>
+            ))}
+          </SelectBoxContainer>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
 export default SelectBox;
@@ -37,22 +70,37 @@ export default SelectBox;
 const TriggerButton = styled.button`
   border: 1px solid black;
   width: 50px;
-  padding: 5px; 
-`
+  height: 50px;
+`;
 
 const SelectBoxContainer = styled.div`
   border: 1px solid black;
   width: 180px;
-`
+`;
+
+const ToolTip = styled.span`
+  display: none;
+  position: absolute;
+  padding: 10px;
+  background-color: gray;
+  color: white;
+  font-size: 12px;
+  max-width: 180px;
+`;
 
 const SelectBoxButton = styled.button`
   border: 1px solid white;
   border: none;
   background-color: white;
   width: 180px;
-  padding: 5px; 
+  padding: 5px;
+  font-size: 15px;
   &:hover {
-    /* border: 1px solid gray; */
     background-color: gainsboro;
   }
-`
+  &:hover ${ToolTip} {
+    display: block;
+    top: 7.5%;
+    left: 200px;
+  }
+`;
