13307200392eabbd9ce41be709805e02151aa0ad
na_0_i <nayoi1oo5@gmail.com>
2022-03-27 23:54:25 +0900

    add: modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import SelectBox from "./SelectBox";
 import Sidebar from "./Sidebar";
+import ToolbarModal from "./ToolbarModal";
 
 function App() {
+  const [openToolbar, setOpenToolbar] = useState(false);
+
   return (
     <div className="App">
       <Sidebar></Sidebar>
       <SelectBox></SelectBox>
+      <>
+        {openToolbar ? (
+          <ToolbarModal openToolbar={setOpenToolbar}></ToolbarModal>
+        ) : (
+          <></>
+        )}{" "}
+      </>
     </div>
   );
 }
---
+++ src/ToolbarModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToolbarModal = () => {
+  return <div>툴바모달</div>;
+};
+
+export default ToolbarModal;
