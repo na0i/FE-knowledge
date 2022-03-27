3d2c236f38b17495859820b61590f791f31dc79e
na_0_i <nayoi1oo5@gmail.com>
2022-03-27 21:52:09 +0900

    add: tooltip

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -22,20 +22,6 @@ const sidebarMenus = [
         desc: "음성으로 검색하는 기능이 필요한 경우 사용해 보세요:)",
       },
     ],
-    desc: [
-      {
-        id: 0,
-        value: "음성을 입력하면 자동으로 텍스트로 변환해줘요:)",
-      },
-      {
-        id: 1,
-        value: "영상 음성을 분석해 ai가 자동으로 자막을 생성해줘요:)",
-      },
-      {
-        id: 2,
-        value: "음성으로 검색하는 기능이 필요한 경우 사용해 보세요:)",
-      },
-    ],
   },
   {
     id: 1,
@@ -50,9 +36,21 @@ const sidebarMenus = [
     id: 2,
     name: "SST2",
     items: [
-      { id: 0, value: "음성 텍스트로 받아쓰기" },
-      { id: 1, value: "영상 자막 생성하기" },
-      { id: 2, value: "음성으로 검색하기" },
+      {
+        id: 0,
+        value: "음성 텍스트로 받아쓰기",
+        desc: "음성을 입력하면 자동으로 텍스트로 변환해줘요:)",
+      },
+      {
+        id: 1,
+        value: "영상 자막 생성하기",
+        desc: "영상 음성을 분석해 ai가 자동으로 자막을 생성해줘요:)",
+      },
+      {
+        id: 2,
+        value: "음성으로 검색하기",
+        desc: "음성으로 검색하는 기능이 필요한 경우 사용해 보세요:)",
+      },
     ],
   },
   {
@@ -75,7 +73,17 @@ const Sidebar = () => {
             <MenuButton key={menu.id}>{menu.name}</MenuButton>
             <MenuDropdowns>
               {menu.items?.map((item) => (
-                <MenuDropdown>{item.value}</MenuDropdown>
+                <MenuDropdown>
+                  <>{item.value}</>
+                  {item.desc ? (
+                    <Tooltip>
+                      <>{item.desc}</>
+                      <TooltipCloseBtn>x</TooltipCloseBtn>
+                    </Tooltip>
+                  ) : (
+                    <></>
+                  )}
+                </MenuDropdown>
               ))}
             </MenuDropdowns>
           </MenuDiv>
@@ -97,12 +105,48 @@ const Side = styled.div`
   border-right: 1px solid gray;
 `;
 
+const Tooltip = styled.div`
+  display: none;
+  position: absolute;
+  height: 50px;
+  width: 200px;
+  left: 110%;
+  border: 1px solid #2c2c2c;
+  background-color: #2c2c2c;
+  color: white;
+  font-size: 0.9rem;
+  padding: 5px;
+  justify-content: space-between;
+  &::after {
+    border-top: 5px solid transparent;
+    border-left: 0px solid transparent;
+    border-right: 10px solid #2c2c2c;
+    border-bottom: 5px solid transparent;
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: -10px;
+  }
+`;
+
+const TooltipCloseBtn = styled.button`
+  text-align: center;
+  height: 1.2rem;
+  border: transparent;
+  background-color: transparent;
+  color: white;
+  cursor: pointer;
+`;
+
 const MenuDropdown = styled.div`
   background-color: white;
   padding: 10px;
   &:hover {
     background-color: #d8d8d8;
     cursor: default;
+  }
+  &:hover ${Tooltip} {
+    display: flex;
   }
 `;
 
@@ -136,5 +180,3 @@ const MenuButton = styled.button`
   height: 80px;
   margin: 10px;
 `;
-
-const MenuTooltips = styled.div``;
